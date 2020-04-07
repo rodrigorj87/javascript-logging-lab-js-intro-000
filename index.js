5a9573804e76a0dbc0b443792b157a22831b539e
Rodrigo Moraes <rodrigorj87@gmail.com>
2020-04-07 18:29:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-console.error(HELP me!);
+console.error(Your password is incorrect.);
+console.log(Please introduce your card in the ATM.);
+console.warn(WARNING. Last chance to sign-in to your account.);
